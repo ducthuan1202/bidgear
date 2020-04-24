7fe55d32099d5bdd9101cf3aa09bf38127812559
Brain Nguyen <thuan.nguyen1@ntq-solution.com.vn>
2020-04-24 14:06:55 +0700

    update code

--- bidgear.go
+++ bidgear.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-func Contact(){
+func Contact() string{
 	return "3rd Floor, No. 1, Block 10A, Trung Yen 3 Street, Trung Hoa Ward, Cau Giay District, Hanoi, Vietnam. Telephone: (+84) 243-200-4426"
 }
 
